853923c1879ad1a1dc0858c296dbfae5c95fde24
itzmegas <megasgnr@gmail.com>
2026-01-18 22:03:48 +0100

    update wrangler deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,14 +8,15 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    imageService: "compile",
+  }),
 
   vite: {
     plugins: [tailwindcss()],
   },
 
   integrations: [react()],
-
   image: {
     domains: ["geo-media.beatport.com", "backend.beatport.com"],
   },
